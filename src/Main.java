aa25bfcb86aea9bf0ed28ab3f8a3c7bfd9ae516f
Arsen Girinov <amateurlib@yandex.ru>
2021-05-25 18:01:11 +0300

    task2

--- src/Main.java
+++ src/Main.java
@@ -3,56 +3,46 @@ import java.util.concurrent.*;
 import java.util.stream.IntStream;
 
 public class Main {
+    private static final int SIZE_MAP = 700;
+    private static final int COUNT_THREAD = 4;
 
     public static void main(String[] args) throws InterruptedException {
-        final int SIZE_MAP = 7000000;
-        final int COUNT_THREAD = 4;
 
-        ConcurrentHashMap<Integer, Integer> concurrentHashMap = new ConcurrentHashMap();
+        ConcurrentHashMap<Integer, Integer> concurrentHashMap = new ConcurrentHashMap<>();
         Map<Integer, Integer> hashMap = Collections.synchronizedMap(new HashMap<>());
 
+        addMap(concurrentHashMap, "ConcurrentHashMap");
+        addMap(hashMap, "HashMap");
 
-        ExecutorService threadPollPut = Executors.newFixedThreadPool(COUNT_THREAD);
-
-        long startTime = System.currentTimeMillis();
-        IntStream.range(0, SIZE_MAP).forEach(i -> threadPollPut.submit(() -> concurrentHashMap.put(i, i)));
-        while (concurrentHashMap.size() != SIZE_MAP){
-        }
-        long finishTime = System.currentTimeMillis();
-        System.out.println("Запись ConcurrentHashMap " + (finishTime - startTime) + " ms.");
-
-        startTime = System.currentTimeMillis();
-        IntStream.range(0, SIZE_MAP).forEach(i -> threadPollPut.submit(() -> hashMap.put(i,i)));
-        while (hashMap.size() != SIZE_MAP){
-        }
-        finishTime = System.currentTimeMillis();
-        System.out.println("Запись HashMap " + (finishTime-startTime) + " ms.");
-        threadPollPut.shutdown();
-
-        Runnable getHashMap = () -> {
-            for (int i = 0; i < SIZE_MAP; i++){
-                hashMap.get(i);
-            }
-        };
-        Runnable getConcurrentHashMap = () -> {
-            for (int i = 0; i < SIZE_MAP; i++){
-                concurrentHashMap.get(i);
-            }
-        };
-
-        getMap(getHashMap, "HashMap");
-        getMap(getConcurrentHashMap, "ConcurrentHashMap");
+        getMap(hashMap, "HashMap");
+        getMap(concurrentHashMap, "ConcurrentHashMap");
     }
 
-    private static void getMap(Runnable run, String typeMap) throws InterruptedException {
-        long startTime;
-        long finishTime;
-        Thread threadGet1 = new Thread(run);
-        Thread threadGet2 = new Thread(run);
-        Thread threadGet3 = new Thread(run);
-        Thread threadGet4 = new Thread(run);
+    private static void addMap(Map<Integer, Integer> map,  String typeMap) {
+        ExecutorService threadPollPut = Executors.newFixedThreadPool(COUNT_THREAD);
+        long startTime = System.currentTimeMillis();
+        IntStream.range(0, SIZE_MAP).forEach(i -> threadPollPut.submit(() -> map.put(i, i)));
+        while (map.size() != SIZE_MAP){
+        }
+        long finishTime = System.currentTimeMillis();
+        System.out.printf("Запись %s %d ms. \n", typeMap, (finishTime - startTime));
+        threadPollPut.shutdown();
+    }
 
-        startTime = System.currentTimeMillis();
+    private static void getMap(Map<Integer, Integer> map, String typeMap) throws InterruptedException {
+
+        Runnable get = () -> {
+            for (int i = 0; i < SIZE_MAP; i++){
+                map.get(i);
+            }
+        };
+
+        Thread threadGet1 = new Thread(get);
+        Thread threadGet2 = new Thread(get);
+        Thread threadGet3 = new Thread(get);
+        Thread threadGet4 = new Thread(get);
+
+        long startTime = System.currentTimeMillis();
         threadGet1.start();
         threadGet2.start();
         threadGet3.start();
@@ -61,7 +51,7 @@ public class Main {
         threadGet2.join();
         threadGet3.join();
         threadGet4.join();
-        finishTime = System.currentTimeMillis();
+        long finishTime = System.currentTimeMillis();
         System.out.printf("Чтение %s %d ms. \n", typeMap, (finishTime - startTime));
     }
 
